7ac28446ad4d21e3465e565d359df5dd273f7bbc
Edaturannn <129127812+Edaturannn@users.noreply.github.com>
2023-04-01 03:00:17 +0300

    C# Form Application da Toplama ve Çıkarma
    Textbox dan girilen iki sayının toplamını ve farkını bulan küçük bir uygulama....

---
+++ Ders15/Ders15/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double sayi1, sayi2, topla = 0, cikar = 0;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            listBox1.Items.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cikar = sayi1 - sayi2;
+            listBox1.Items.Add("ÇIKARMA= " + cikar);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            sayi2 = Convert.ToDouble(textBox2.Text);
+            topla = sayi1 + sayi2;
+            listBox1.Items.Add("TOPLAM= " + topla);
+        }
+    }
+}
